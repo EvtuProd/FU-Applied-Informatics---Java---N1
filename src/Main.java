bc5931b82652e17a846fc7f7ffaf1c3dd68ab77a
EvtuProd <mail@tuhachewscky.ru>
2024-05-02 18:37:53 +0300

    Создан класс TreeNode

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class TreeNode {
+    private int id;
+    private TreeNode parent;
+    private List<TreeNode> children;
+
+    public TreeNode(int id) {
+        this.id = id;
+        this.parent = null;
+        this.children = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    public boolean isLeaf() {
+        return children.isEmpty();
+    }
+
+
+    public boolean isRoot() {
+        return parent == null;
     }
 }
